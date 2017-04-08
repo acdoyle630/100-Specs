ad549645e50d35d0f7c84ff7f046bd1ebc6a0910
Adam <acdoyle630@gmail.com>
2017-04-07 19:04:02 -1000

    scmList

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -247,7 +247,7 @@ const livingOrganismClassification = ['Kingdom', 'Phylum', 'Class', 'Order','Fam
  *
  */
 
-
+const scmList = ['git', 'svn', 'mercurial', 'bazaar', 'cvs'];
 /* Step 19
  *
  * Define a literal object named "beers" with
